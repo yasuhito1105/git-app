21230ef03587de71039082083a16a8d941cc1d4d
yasuhito1105 <yatti10@gmail.com>
2024-11-05 18:05:52 +0900

    posts#indexへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root 'posts#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
